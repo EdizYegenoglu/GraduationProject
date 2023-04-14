cd9e4327e9aa211b3e506e440ec2f8faa775cd48
EdizYegenoglu <Ediz.yegenoglu@hva.nl>
2023-04-14 16:54:28 +0200

    Camera Update
    Picture development time added

--- VR_Photography/Assets/Scripts/Photo.cs
+++ VR_Photography/Assets/Scripts/Photo.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Photo : MonoBehaviour
 {
     public MeshRenderer imageRenderer = null;
+    Color colorStart = Color.black;
+    Color colorEnd = Color.white;
+    public float fadeDuration = 1.0f;
+
 
     private Collider currentCollider = null;
     // private ApplyPhysics applyPhysics = null;
@@ -17,7 +21,8 @@ public class Photo : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(EjectOverSeconds(1.5f));
+        StartCoroutine(EjectOverSeconds(1.5f));        
+        StartCoroutine(FadeObject());
     }
 
     public IEnumerator EjectOverSeconds(float seconds)
@@ -37,9 +42,18 @@ public class Photo : MonoBehaviour
         currentCollider.enabled = true;
     }
 
+    public IEnumerator FadeObject(){
+        float t = 0.0f;
+        while (t < fadeDuration){
+            t += Time.deltaTime;
+            imageRenderer.material.color = Color.Lerp(colorStart, colorEnd, t / fadeDuration);
+            yield return null;
+        }
+    }
+
     public void SetImage(Texture2D texture)
     {
-        imageRenderer.material.color = Color.white;
+        // imageRenderer.material.color = Color.white;
         imageRenderer.material.mainTexture = texture;
     }
 
